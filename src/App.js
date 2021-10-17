da706b3a28aa67c6e0f9a8dfd14bc6b29662b5c9
Vahid Al <vahidtheal@gmail.com>
2021-10-17 15:24:30 +0330

    fetch apis. get method.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Switch, Link, Route, useLocation } from 'react-router-dom'
+import axios from 'axios'
 
 import './App.css';
 
@@ -62,11 +63,83 @@ const Layout = (props) => {
   )
 }
 
+const PAGE_SIZE = 10
+
 const Login = (props) => {
+  const [instruments, setInstruments] = useState([])
+  const [instrumentsCount, setInstrumentsCount] = useState(0)
+  const [searchQuery, setSearchQuery] = useState('')
+  const [page, setPage] = useState(1)
+
+  useEffect(() => {
+    axios.get(`https://api.pabla.ir/api/v1/instruments/?search=${searchQuery}&limit=${PAGE_SIZE}&offset=${(page - 1) * PAGE_SIZE}`).then(res => {
+      setInstruments(res.data.results)
+      setInstrumentsCount(res.data.count)
+    })
+  }, [searchQuery, page])
+
+  console.log(page)
 
   return (
-    <div>
-      This is login screen
+    <div style={{
+      marginTop: 20,
+    }}>
+      <input 
+        placeholder='Search instrument name...'
+        style={{
+          padding: '10px 15px',
+          fontSize: 17,
+          borderRadius: 10,
+          border: '1px solid #999',
+        }}
+        onChange={e => setSearchQuery(e.target.value)}
+      />
+      <h3>Searching for: {searchQuery}</h3>
+      
+      <div style={{
+        width: '100vw',
+        minHeight: '100vh',
+        padding: 20,
+        backgroundColor: 'powderblue',
+        marginTop: 20,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+        {
+          instruments.map(i => (
+            <div style={{
+              padding: 10,
+              border: '1px solid #222',
+              width: 100,
+              marginBottom: 10,
+            }}>
+              <h4>{i.name} . {i.name_fa}</h4>
+              <img src={i.icon} style={{
+                width: 100,
+                height: 100,
+              }} />
+            </div>
+          ))
+        }
+      </div>
+      <div>
+        {
+          Array(Math.ceil(instrumentsCount / PAGE_SIZE)).fill(null).map((item, index) => (
+            <button
+              onClick={() => setPage(index + 1)}
+              style={{
+                backgroundColor: (page === index + 1) ? 'orange' : 'white',
+                border: '1px solid #999',
+                margin: 5,
+                borderRadius: 5,
+                padding: 10,
+              }}
+            >{index + 1}</button>
+          ))
+        }
+      </div>
     </div>
   )
 }
@@ -97,7 +170,7 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Link></Link>
+        <Link to='/login'>Login</Link>
 
         <Switch>
           <Route exact={true} path='/login'>
